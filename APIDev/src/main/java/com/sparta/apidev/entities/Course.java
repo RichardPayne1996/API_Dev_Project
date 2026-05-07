48f92c321eac6920cad61e38a60ed760a7cbc0dc
Reece776 <jacobr2609@gmail.com>
2026-05-07 15:44:24 +0100

    update course entity

--- APIDev/src/main/java/com/sparta/apidev/entities/Course.java
+++ APIDev/src/main/java/com/sparta/apidev/entities/Course.java
@@ -1,4 +1,54 @@
 package com.sparta.apidev.entities;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "courses")
 public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_id", nullable = false)
+    private Integer id;
+
+    // Trainee side (student_course table)
+    @ManyToMany(mappedBy = "courses")
+    private Set<Trainee> trainees = new HashSet<>();
+
+    // Trainer side (teaching table)
+    @ManyToMany(mappedBy = "courses")
+    private List<Trainer> trainers = new ArrayList<>();
+
+    public Course() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Set<Trainee> getTrainees() {
+        return trainees;
+    }
+
+    public void setTrainees(Set<Trainee> trainees) {
+        this.trainees = trainees;
+    }
+
+    public List<Trainer> getTrainers() {
+        return trainers;
+    }
+
+    public void setTrainers(List<Trainer> trainers) {
+        this.trainers = trainers;
+    }
 }
+
