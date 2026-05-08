6f2cd8e29b3a8ea0ae7c64e1795f523c941da60e
Richard Payne <rich.payne.1996@gmail.com>
2026-05-08 10:11:41 +0100

    DTO and controller for trainee done

--- APIDev/src/main/java/com/sparta/apidev/dtos/TraineeDTO.java
+++ APIDev/src/main/java/com/sparta/apidev/dtos/TraineeDTO.java
@@ -1,4 +1,49 @@
 package com.sparta.apidev.dtos;
 
 public class TraineeDTO {
+    private int traineeId;
+    private String traineeName;
+    private String traineeEmail;
+    private String traineeDOB;
+    private String traineeTitle;
+
+    public int getTraineeId() {
+        return traineeId;
+    }
+
+    public void setTraineeId(int traineeId) {
+        this.traineeId = traineeId;
+    }
+
+    public String getTraineeName() {
+        return traineeName;
+    }
+
+    public void setTraineeName(String traineeName) {
+        this.traineeName = traineeName;
+    }
+
+    public String getTraineeEmail() {
+        return traineeEmail;
+    }
+
+    public void setTraineeEmail(String traineeEmail) {
+        this.traineeEmail = traineeEmail;
+    }
+
+    public String getTraineeDOB() {
+        return traineeDOB;
+    }
+
+    public void setTraineeDOB(String traineeDOB) {
+        this.traineeDOB = traineeDOB;
+    }
+
+    public String getTraineeTitle() {
+        return traineeTitle;
+    }
+
+    public void setTraineeTitle(String traineeTitle) {
+        this.traineeTitle = traineeTitle;
+    }
 }
--- APIDev/src/main/java/com/sparta/apidev/services/TraineeService.java
+++ APIDev/src/main/java/com/sparta/apidev/services/TraineeService.java
@@ -1,4 +1,66 @@
 package com.sparta.apidev.services;
 
+import com.sparta.apidev.dtos.TraineeDTO;
+import com.sparta.apidev.dtos.TraineeMapper;
+import com.sparta.apidev.entities.Trainee;
+import com.sparta.apidev.repositories.TraineeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class TraineeService {
+
+    private final TraineeRepository traineeRepository;
+    private final TraineeMapper traineeMapper;
+
+    @Autowired
+    public TraineeService(TraineeRepository traineeRepository, TraineeMapper traineeMapper) {
+        if (traineeRepository == null || traineeMapper == null) {
+            throw new  IllegalArgumentException();
+        }
+        this.traineeRepository = traineeRepository;
+        this.traineeMapper = traineeMapper;
+    }
+
+    public List<TraineeDTO> getAllTrainees() {
+        return traineeRepository.findAll().stream().map(traineeMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public TraineeDTO getTraineeById(Integer id) {
+        return traineeRepository.findById(id).map(traineeMapper::toDTO)
+                .orElse(null);
+    }
+
+    public TraineeDTO saveTrainee(Trainee trainee) {
+        return traineeMapper.toDTO(traineeRepository.save(trainee));
+    }
+
+    public boolean deleteTrainee(Integer id) {
+        if (traineeRepository.existsById(id)) {
+            traineeRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    public TraineeDTO updateCustomer(int id, TraineeDTO dto) {
+
+        Trainee trainee = traineeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Trainee not found"));
+
+        trainee.setTraineeName(dto.getTraineeName());
+        trainee.setDoB(dto.getTraineeDOB());
+        trainee.setEmail(dto.getTraineeEmail());
+        trainee.setID(dto.getTraineeId());
+        trainee.setTitle(dto.getTraineeTitle());
+
+        Trainee updated = traineeRepository.save(trainee);
+
+        return traineeMapper.toDTO(updated);
+    }
+
 }
