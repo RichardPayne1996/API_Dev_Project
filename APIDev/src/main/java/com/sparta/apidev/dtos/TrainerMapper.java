76982a9977d42e95d8aba603fd140dc7cd1f2912
Ezra Duncan <etduncan03@gmail.com>
2026-05-19 12:25:06 +0100

    assigned trainer to courses

--- APIDev/src/main/java/com/sparta/apidev/dtos/TrainerMapper.java
+++ APIDev/src/main/java/com/sparta/apidev/dtos/TrainerMapper.java
@@ -4,9 +4,15 @@ import com.sparta.apidev.entities.Course;
 import com.sparta.apidev.entities.Trainer;
 import com.sparta.apidev.enums.Role;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface TrainerMapper {
+
+    @Mapping(target = "courses", source = "teacherCourse")
     TrainerDTO toDTO(Trainer trainer);
 
     Trainer toEntity(TrainerDTO trainerDTO);
@@ -18,5 +24,12 @@ public interface TrainerMapper {
     // String -> Enum
     default Role map(String role) {
         return role != null ? Role.valueOf(role.toUpperCase()) : null;
+    }
+
+    default Set<CourseDTO> mapCourses(Set<Course> courses) {
+        if (courses == null) return null;
+        return courses.stream()
+                .map(c -> new CourseDTO(c.getId(), c.getCourseName(), c.getDescription()))
+                .collect(Collectors.toSet());
     }
 }
